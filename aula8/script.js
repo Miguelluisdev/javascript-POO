4ba154a9dcc20588505095af9b45864f05e2a827
Miguel luis <miguelluisataideferreira68@gmail.com>
2023-08-28 14:31:00 -0300

    Create script.js

---
+++ aula8/script.js
@@ -0,0 +1,63 @@
+class Carro{
+  constructor(tipo,estagioTurbo){
+    this.turbo = new Turbo(estagioTurbo)
+    if (tipo === 1){
+      this.velmax = 120 
+      this.nome = "normal"
+    } else if (tipo === 2){
+      this.velmax = 180
+      this.nome = "sportivo"
+    } else if (tipo === 3){
+      this.velmax = 200 
+      this.nome = "megaesportivo"
+    }
+    this.velmax += this.turbo.pot
+  }
+  info(){
+    console.log(`
+    nome ${this.nome}
+    velmax ${this.velmax}
+    turbo ${this.turbo}
+    tipo ${this.tipo}
+    ------------------
+    `)
+  }
+}
+
+class Turbo{
+  constructor(e){
+      if (e === 0){
+        this.pot = 0
+      } else if (e === 1){
+        this.pot = 50
+      } else if (e === 3){
+        this.pot = 75
+      } else if (e === 100){
+        this.pot = 100
+      }
+  }
+}
+
+class CarroEspecial extends Carro{
+  constructor(estagioTurbo){
+    super(4,estagioTurbo)
+    this.nome = "carro especial"
+    this.velmax = 300 + this.turbo.pot
+  }
+  info(){
+    console.log(`
+    nome ${this.nome}
+    velmax ${this.velmax}
+    turbo ${this.turbo}
+    ------------------
+    `)
+    // super.info()
+  }
+}
+const c1 = new Carro(1,0)
+const c2 =  new Carro(2,1)
+const c3 = new CarroEspecial(3)
+
+c1.info()
+c2.info()
+c3.info()
